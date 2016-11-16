303913276a827e888d8e930fa3c9fd3fe7ec3196
Sinan Kaplan <kaplansinan@users.noreply.github.com>
2016-11-16 16:39:26 +0200

    Delete non_linear_log.m

--- StatisticalAnalysis/non_linear_log.m
+++
@@ -1,9 +0,0 @@
-function ss = non_linear_log( theta,data )
-%NON_LINEAR_LOG Summary of this function goes here
-%   Detailed explanation goes here
-    x = data(:,1);
-    x2 = data(:,2);
-    y = data(:,3);
-    ss = sum((y -(theta(1)+theta(2)*x+theta(3)*x2)).^2);
-
-end
